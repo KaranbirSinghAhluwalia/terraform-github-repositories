63afdb2a3201cc1dc84d91c076f304d142129fcf
karanbirsingh93@outlook.com <Karan@9600>
2025-08-15 02:23:26 +0530

    updating code

--- modules/github-repo/provider.tf
+++ modules/github-repo/provider.tf
@@ -14,6 +14,6 @@ provider "github" {
   app_auth {
     id              = 1782237
     installation_id = 80950058
-    pem_file        = "${get_env("GITHUB_WORKSPACE")}/app.pem"
+    pem_file        = "${path.root}/app.pem"
   }
 }
