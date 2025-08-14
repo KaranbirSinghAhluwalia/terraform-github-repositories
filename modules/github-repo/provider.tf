4ab553aae43c567114d06a8c0c8643148bf609e7
karanbirsingh93@outlook.com <Karan@9600>
2025-08-15 00:09:01 +0530

    updating location

--- modules/github-repo/provider.tf
+++ modules/github-repo/provider.tf
@@ -14,6 +14,6 @@ provider "github" {
   app_auth {
     id              = env("GITHUB_APP_ID")
     installation_id = env("GITHUB_APP_INSTALLATION_ID")
-    pem_file        = env("GITHUB_APP_PEM_FILE")
+    pem_file        = "../../app.pem"
   }
 }
