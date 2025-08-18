a08fc26f31b0469b005c14eb25f2edfe45cd2016
karanbirsingh93@outlook.com <Karan@9600>
2025-08-18 05:38:36 +0530

    updating code

--- modules/github-repo/provider.tf
+++ modules/github-repo/provider.tf
@@ -10,10 +10,5 @@ terraform {
 
 provider "github" {
   owner = env("GITHUB_OWNER")
-
-  app_auth {
-    id              = 1782237
-    installation_id = 80950058
-    pem_file        = var.app_pem_path != "" ? "../../app.pem" : getenv("APP_PEM_PATH")
-  }
+  app_auth {}
 }
